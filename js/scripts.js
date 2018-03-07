12b5a81d9e05f9d53f04a53f1aba47bca615e6d2
leskeylevy <leskeylevy@gmail.com>
2018-03-07 10:17:19 +0300

    js edditing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //business logic
-function Contact(name, id, service, location, contact, email, website) {
+function Offer(name, id, service, location, contact, email, website) {
   this.fullName = name;
   this.idNumber = id;
   this.serviceOffered = service;
@@ -11,19 +11,18 @@ function Contact(name, id, service, location, contact, email, website) {
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("form#new-Offer").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-full-name").val();
-    var inputtedLastName = $("input#new-id").val();
-    var inputtedFirstName = $("input#new-service").val();
-    var inputtedLastName = $("input#new-location").val();
-    var inputtedFirstName = $("input#new-contact").val();
-    var inputtedLastName = $("input#new-email").val();
-    var inputtedFirstName = $("input#new-").val();
-    var inputtedLastName = $("input#new-id").val();
+    var inputtedFullName = $("input#new-full-name").val();
+    var inputtedIdNumber = $("input#new-id").val();
+    var inputtedServiceOffered = $("input#new-service").val();
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedPersonalContact = $("input#new-contact").val();
+    var inputtedEmailAddress = $("input#new-email").val();
+    var inputtedWebsiteLink = $("input#new-website").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    console.log(inputtedFirstName);
+    var newContact = new Offer(inputtedFullName, inputtedIdNumber, inputtedServiceOffered, inputtedLocation, inputtedPersonalContact, inputtedEmailAddress, inputtedWebsiteLink);
+    //console.log(inputtedFullName, inputtedServiceOffered, inputtedPersonalContact);
   });
 });
